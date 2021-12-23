96ade2370e1ddfdbfba112b657dd6160d58fee44
Cyn <SlyCedix@gmail.com>
2021-12-23 13:39:59 -0500

    Clean up typos

--- updateDefs.js
+++ updateDefs.js
@@ -1,4 +1,4 @@
-const https = require("https");
+const https = require("https")
 const fs = require("fs")
 
 const url = 'https://raw.githubusercontent.com/danielyxie/bitburner/dev/src/ScriptEditor/NetscriptDefinitions.d.ts'
